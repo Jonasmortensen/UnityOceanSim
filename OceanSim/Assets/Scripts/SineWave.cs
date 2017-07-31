1ef5d4eeb740d5be9fa7a73c6504b1f692c13372
Jonas Mortensen <Jonasmortensen3@gmail.com>
2017-07-31 12:58:47 +0200

    Added sineWAve

---
+++ OceanSim/Assets/Scripts/SineWave.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SineWave : MonoBehaviour, IWave {
+
+    public float WaveLength = 1.0f;
+    public float Speed = 1.0f;
+    public float Amplitude = 1.0f;
+    public Vector2 Direction = Vector2.left;
+
+    private float _frequency;
+    private float _phaseConstant;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    public Vector3 getPositionOffset(float x, float z, float time) {
+        var position = new Vector2(x, z);
+        var y = Amplitude * Mathf.Sin(Vector2.Dot(Direction, position) * _frequency + time * _phaseConstant);
+        return new Vector3(x, y, z);
+    }
+}
