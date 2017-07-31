35dca13fec830a0f02c300c3cf1ada67a8d37369
Jonas Mortensen <Jonasmortensen3@gmail.com>
2017-07-31 14:51:44 +0200

    three waves

--- OceanSim/Assets/Scripts/GerstnerWave.cs
+++ OceanSim/Assets/Scripts/GerstnerWave.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class GerstnerWave : MonoBehaviour, IWave {
 
-    public float WaveLength = 4.0f;
+    public float WaveLength = 10.0f;
     public float Speed = 1.0f;
     public float Amplitude = 1.0f;
     public Vector2 Direction = Vector2.left;
@@ -29,7 +29,9 @@ public class GerstnerWave : MonoBehaviour, IWave {
         _frequency = 2 / WaveLength;
         Speed = Mathf.Sqrt(9.8f * ((2 * Mathf.PI) / WaveLength));
         _phaseConstant = Speed * _frequency;
-        _q = 0.5f / (_frequency * Amplitude);
+        _q = 1.0f / (_frequency * Amplitude * 3);
+
+        Debug.Log("Set speed = " + Speed);
     }
 
     // Use this for initialization
--- OceanSim/Assets/Scripts/OceanSurface.cs
+++ OceanSim/Assets/Scripts/OceanSurface.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class OceanSurface : MonoBehaviour {
 
-    public IWave wave;
+    public List<IWave> Waves;
 
     private Mesh _mesh;
     private Vector3[] _originalVertices;
     private Vector3[] _modifiedVertices;
     private float elapsedTime;
+    private Matrix4x4 localToWorld;
+    private Matrix4x4 worldToLocal;
 
 	// Use this for initialization
 	void Start () {
@@ -17,13 +19,34 @@ public class OceanSurface : MonoBehaviour {
         _originalVertices = _mesh.vertices;
         _modifiedVertices = _mesh.vertices;
 
-        wave = new GerstnerWave();//new SineWave();
-        wave.UpdateConfiguration();
-        
-	}
+        Waves = new List<IWave>();
+
+        var wave1 = new GerstnerWave();//new SineWave();
+        wave1.UpdateConfiguration();
+
+        var wave2 = new GerstnerWave();//new SineWave();
+        wave2.WaveLength = 5.0f;
+        wave2.Amplitude = 0.25f;
+        var direction = new Vector2(-3.0f, -1.0f);
+        wave2.Direction = direction.normalized;
+        wave2.UpdateConfiguration();
+
+        var wave3 = new GerstnerWave();//new SineWave();
+        wave3.WaveLength = 2.0f;
+        wave3.Amplitude = 0.1f;
+        var direction3 = new Vector2(-3.0f, 2.0f);
+        wave3.Direction = direction3.normalized;
+        wave3.UpdateConfiguration();
+
+        Waves.Add(wave1);
+        Waves.Add(wave2);
+        Waves.Add(wave3);
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        localToWorld = transform.localToWorldMatrix;
+        worldToLocal = transform.worldToLocalMatrix;
         elapsedTime += Time.deltaTime;
 
         updateVertices(_originalVertices, _modifiedVertices, elapsedTime);
@@ -37,8 +60,14 @@ public class OceanSurface : MonoBehaviour {
     private void updateVertices(Vector3[] original, Vector3[] toUpdate, float time) {
         int i = 0;
         while(i < original.Length) {
-            Vector3 currentVertex = original[i];
-            toUpdate[i] = currentVertex + wave.getPositionOffset(currentVertex.x, currentVertex.z, time);
+            Vector3 posOffset = Vector3.zero;
+            Vector3 currentVertex = localToWorld.MultiplyPoint3x4(original[i]);
+
+            for (int j = 0; j < Waves.Count; j++) {
+                posOffset += Waves[j].getPositionOffset(currentVertex.x, currentVertex.z, time);
+            }
+
+            toUpdate[i] = worldToLocal.MultiplyPoint3x4(currentVertex + posOffset);
             i++;
         }
     }
