77d49a57d0481211e85f724553d1e7fcdbc3f5c3
Jonas Mortensen <Jonasmortensen3@gmail.com>
2017-08-04 00:54:08 +0200

    some playing

---
+++ OceanSim/Assets/Scripts/Camera/PlayingCam.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlayingCam : MonoBehaviour {
+
+    public Transform boat;
+
+    public Transform camPos1;
+    public Transform camPos2;
+
+    [Range(0.0f, 1.0f)]
+    public float interpolation;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(camPos1.position, camPos2.position, interpolation);
+        transform.rotation = Quaternion.Lerp(camPos1.rotation, camPos2.rotation, interpolation);
+	}
+}
---
+++ OceanSim/Assets/Scripts/Movement/SinePulse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinePulse : MonoBehaviour {
+    public float speed;
+    public float amount;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = transform.position;
+        pos.y = Mathf.Sin(Time.time * speed) * amount;
+        transform.position = pos;
+		
+	}
+}
